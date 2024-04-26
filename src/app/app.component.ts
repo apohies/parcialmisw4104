7dd7cb2ea84d8e58ee109779ee754b14c65c9306
apohies <ma.moreno2@uniandes.edu.co>
2024-04-25 20:14:18 -0500

    servicio.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AutoService } from './services/auto.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export  class AppComponent implements OnInit 
+{
   title = 'parcialmisw4104';
+
+  constructor(private autoService : AutoService) { }
+
+  ngOnInit(): void {
+   this.loadAutos();
+  }
+
+  loadAutos(){
+    this.autoService.getAutos().subscribe(data => {
+      console.log(data);
+    });
+  }
 }
---
+++ src/app/models/auto.ts
@@ -0,0 +1,11 @@
+export interface Auto{
+
+    id: number;
+    marca: string;
+    linea: string;
+    referencia: number;
+    modelo: string;
+    kilometraje: number;
+    color: string;
+    imagen: string;
+}
