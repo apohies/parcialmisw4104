988ced6194537d5e481c347dc302c583d670bee2
apohies <ma.moreno2@uniandes.edu.co>
2024-04-26 20:29:35 -0500

    base

--- src/app/services/auto.service.ts
+++ src/app/services/auto.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Auto} from '../models/auto';
+import { RouterModule } from '@angular/router';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class AutoService {
   private url = "https://gist.githubusercontent.com/josejbocanegra/17bb8c76405e43655d551a90800c8a81/raw/d41b4acc3457e51e7533fad6d5e9925ee9676457/202212_MISW4104_Grupo1.json";
   constructor(private htpp:HttpClient) { 
